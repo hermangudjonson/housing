91eb2dd19105c63bcfffc124a4483a2a8a97f0c6
Herman Gudjonson <herman.gudjonson@gmail.com>
2023-10-20 13:46:04 -0400

    add validation cv support
    including lightgbm proxy class

--- housing/model.py
+++ housing/model.py
@@ -2,14 +2,21 @@
 Routines for housing model generation and evaluation.
 """
 
+from functools import partial
+from time import time
+
 import numpy as np
 import pandas as pd
+import sklearn.metrics as skm
+import lightgbm as lgbm
+from sklearn.base import BaseEstimator, RegressorMixin, clone, is_classifier
 from sklearn.compose import make_column_selector, make_column_transformer
 from sklearn.decomposition import PCA
 from sklearn.experimental import enable_iterative_imputer  # noqa: F401
 from sklearn.impute import IterativeImputer
 from sklearn.linear_model import BayesianRidge, ElasticNetCV
 from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.model_selection import check_cv
 from sklearn.naive_bayes import CategoricalNB
 from sklearn.pipeline import FeatureUnion, Pipeline, make_pipeline, make_union
 from sklearn.preprocessing import (
@@ -18,6 +25,7 @@ from sklearn.preprocessing import (
     OneHotEncoder,
     OrdinalEncoder,
 )
+from sklearn.utils.metaestimators import _safe_split
 
 from housing import load_prep
 
@@ -222,16 +230,83 @@ def get_gower_dist(X):
     return (dice_dist + man_dist) / 2.0
 
 
-def get_regressor(strategy="passthrough"):
+class LGBMProxy(BaseEstimator, RegressorMixin):
+    """LightGBM wrapper that conforms to sklearn interface
+
+    specifically move callbacks and validation data to initialization
+    rather than needing to be passed during the call to fit itself.
+    Note catboost tries to JSON serialize all parameters (fails for dataframe)
+
+    Parameters
+    ----------
+    callbacks : list, optional
+        lightgbm fit callback functions
+    validation : tuple (X, y), optional
+        validation data, required to use early stopping
+    **params : optional
+        parameters to be passed to LGBMRegressor initialization
+    """
+
+    def __init__(self, callbacks=None, validation=None, **params):
+        self.callbacks = callbacks
+        self.validation = validation
+        # catboost classifier
+        self.estimator_ = lgbm.LGBMRegressor(**params)
+
+    def get_params(self, deep=True):
+        return self.estimator_.get_params(deep) | {
+            "callbacks": self.callbacks,
+            "validation": self.validation,
+        }
+
+    def set_params(self, **params):
+        if "callbacks" in params:
+            self.callbacks = params.pop("callbacks")
+        if "validation" in params:
+            self.validation = params.pop("validation")
+        self.estimator_.set_params(**params)
+
+    def fit(self, X, y):
+        if self.validation is not None:
+            eval_set = [self.validation, (X, y)]
+            eval_names = ["validation", "training"]
+        else:
+            eval_set = None
+            eval_names = None
+        self.estimator_.fit(
+            X,
+            y,
+            eval_set=eval_set,
+            eval_names=eval_names,
+            eval_metric=None,  # defaulting to training objective
+            callbacks=self.callbacks,
+        )
+        return self
+    
+    def __getattr__(self, name):
+        """dispatch other methods to estimator"""
+        return getattr(self.estimator_, name)
+
+def get_regressor(strategy="passthrough", params=None):
+    params = params if params is not None else {}
+
     if strategy == "passthrough":
         return "passthrough"
     elif strategy == "linear":
-        return ElasticNetCV(l1_ratio=0.9)
+        enet_params = {"l1_ratio": 0.9}
+        params = enet_params | params
+        return ElasticNetCV(**params)
+    elif strategy == "lightgbm":
+        return LGBMProxy(**params)
     else:
         raise ValueError(f"unimplemented strategy {strategy}")
 
 
-def get_reg_pipeline(reg_strategy="passthrough", onehot_encode=False, include_pca=True):
+def get_reg_pipeline(
+    reg_strategy="passthrough", reg_params=None, onehot_encode=False, include_pca=True
+):
+    reg_params = reg_params if reg_params is not None else {}
+
     # preprocess, impute, (optional) onehot, regressor
     impute_step = get_imputer()
 
@@ -255,11 +330,177 @@ def get_reg_pipeline(reg_strategy="passthrough", onehot_encode=False, include_pc
             ("impute", impute_step),
             ("PCA", pca_step if include_pca else "passthrough"),
             ("onehot", impute_to_onehot if onehot_encode else "passthrough"),
-            ("regressor", get_regressor(reg_strategy)),
+            ("regressor", get_regressor(strategy=reg_strategy, params=reg_params)),
         ]
     )
     # since we are passing info between steps, we override clone
     reg_pipe.__sklearn_clone__ = lambda: get_reg_pipeline(
-        reg_strategy, onehot_encode, include_pca
+        reg_strategy, reg_params, onehot_encode, include_pca
     )
     return reg_pipe
+
+
+def fit_with_validation(
+    reg_pipe: Pipeline,
+    X_train,
+    y_train,
+    X_valid=None,
+    y_valid=None,
+):
+    """fit routine for regression pipeline.
+
+    fits first stages first in order to pass transformed
+    validation data to final classifier if necessary
+
+    Parameters
+    ----------
+    reg_pipe : Pipeline
+        multi-step pipeline with final step regressor
+    X_train : 2D array_like
+    y_train : 1D array_like
+    X_valid : 2D array_like, optional
+        validation data to be transformed and passed to regressor, by default None
+    y_valid : 1D array_like, optional
+        by default None
+
+    Returns
+    -------
+    reg_pipe : Pipeline, fitted
+    """
+    if X_valid is None:
+        # simple call to fit
+        return reg_pipe.fit(X_train, y_train)
+
+    X_train_pre = reg_pipe[:-1].fit_transform(X_train, y_train)
+    X_valid_pre = reg_pipe[:-1].transform(X_valid)
+
+    # last step has to accept validation as a param
+    reg_pipe[-1].set_params(validation=(X_valid_pre, y_valid))
+    reg_pipe[-1].fit(X_train_pre, y_train)
+
+    return reg_pipe
+
+
+def cv_with_validation(estimator, X, y, cv, callbacks=None):
+    """Perform cross-validation while passing validation data to the estimator
+    in each fold.
+
+    estimator is fit using `fit_with_validation`, and is assumed to be a pipeline that
+    can accept a validation data parameter
+
+    return results dictionary with one key per callback
+    """
+    cv = check_cv(cv, y, classifier=is_classifier(estimator))
+    callbacks = callbacks if callbacks is not None else {}
+
+    result = {k: {} for k in callbacks.keys()}
+    result["train_time"] = {}
+
+    for fold, (train_idx, test_idx) in enumerate(cv.split(X, y)):
+        fold_estimator = clone(estimator)
+        # form a dummy pipeline to make this compatible with single estimators
+        if not isinstance(estimator, Pipeline):
+            fold_estimator = make_pipeline("passthrough", fold_estimator)
+
+        X_train, y_train = _safe_split(fold_estimator, X, y, train_idx)
+        X_valid, y_valid = _safe_split(
+            fold_estimator, X, y, test_idx, train_indices=train_idx
+        )
+
+        start_time = time()
+
+        if "validation" in fold_estimator[-1].get_params():
+            # pass validation data if validation param exists in final pipeline step
+            fit_with_validation(fold_estimator, X_train, y_train, X_valid, y_valid)
+        else:
+            fit_with_validation(fold_estimator, X_train, y_train)
+
+        fit_time = time() - start_time
+        result["train_time"][fold] = fit_time
+
+        if not isinstance(estimator, Pipeline):
+            # revert back to single estimator for evaluation
+            fold_estimator = fold_estimator[-1]
+
+        for k, func in callbacks.items():
+            result[k][fold] = func(
+                fold=fold,
+                estimator=fold_estimator,
+                indices=(train_idx, test_idx),
+                train_data=(X_train, y_train),
+                test_data=(X_valid, y_valid),
+            )
+    return result
+
+
+def _score_regressor(reg, X, y, eval_name=None):
+    """score fitted classifier for common metrics.
+
+    Applies metrics that compare true targets to predicted targets.
+    """
+    other_evals = {
+        "rmse": partial(skm.mean_squared_error, squared=False),
+        "r2": skm.r2_score,
+        "mae": skm.mean_absolute_error,
+        "mape": skm.mean_absolute_percentage_error,
+    }
+
+    y_pred = reg.predict(X)
+    eval_vals = {s: f(y_pred, y) for s, f in other_evals.items()}
+    if eval_name is not None:
+        eval_vals["eval_set"] = eval_name
+
+    return pd.DataFrame(pd.Series(eval_vals)).T
+
+
+def _score_train(*, estimator, train_data, **kwargs):
+    X, y = train_data
+    return _score_regressor(estimator, X, y, eval_name="train")
+
+
+def _score_test(*, estimator, test_data, **kwargs):
+    X, y = test_data
+    return _score_regressor(estimator, X, y, eval_name="test")
+
+
+def _predict_test(*, estimator, test_data, **kwargs):
+    X, y = test_data
+    return estimator.predict(X)
+
+
+def lgbm_fit_metrics(*, estimator, **kwargs):
+    """Return fit metrics for fitted LightGBM model"""
+    clf = estimator["clf"]
+    best_ntree = clf.best_iteration_ if clf.best_iteration_ else clf.n_estimators
+    best_idx = best_ntree - 1
+
+    lgbm_evals = {
+        **{"train_" + k: v[best_idx] for k, v in clf.evals_result_["training"].items()},
+        **{
+            "test_" + k: v[best_idx] for k, v in clf.evals_result_["validation"].items()
+        },
+        **{"best_ntree": best_ntree},
+    }
+    return pd.DataFrame(pd.Series(lgbm_evals)).T
+
+
+def common_cv_callbacks():
+    """Generates dictionary of common CV callback functions for cv_with_validation.
+
+    includes:
+        - fitted estimator
+        - fold indices
+        - evaluation metrics on training data
+        - evaluation metrics on test data
+        - test predictions
+        - test target data
+    """
+    callbacks = {
+        "estimator": lambda *, estimator, **kw: estimator,
+        "indices": lambda *, indices, **kw: indices,
+        "eval_train": _score_train,
+        "eval_test": _score_test,
+        "predict_test": _predict_test,
+        "y_test": lambda *, test_data, **kw: test_data[1],
+    }
+    return callbacks
